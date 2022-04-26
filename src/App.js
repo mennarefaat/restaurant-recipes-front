88687323eef58878c5547698857e14fdcf908e3f
menna refaat <menna.ref3t@gmail.com>
2022-04-27 01:11:28 +0200

    Add Project Structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import getDesignTokens from './components/them'
+import Box from '@mui/material/Box';
 import './App.css';
+
+
+const darkModeTheme = createTheme(getDesignTokens('dark'));
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkModeTheme}>
+      <Box sx={{
+
+        bgcolor: 'background.default',
+        color: 'text.primary',
+      }}>
+
+        <div className="App">
+        
+        </div>
+      </Box>
+    </ThemeProvider>
   );
 }
 
